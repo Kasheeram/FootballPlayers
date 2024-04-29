ce5a97e629ea14c0603afedfa929468ce15e2eb9
kashee-lv <kasheeram.kushwaha@letsventure.com>
2024-04-29 23:54:50 +0530

    Initial Commit

---
+++ FootballPlayers/FootballPlayersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballPlayersApp.swift
+//  FootballPlayers
+//
+//  Created by kashee on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct FootballPlayersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
